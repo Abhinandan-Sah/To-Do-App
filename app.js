ff65e316622481c849f14a43a78bb7f3684fc71f
Abhinandan Sah <118913466+Abhinandan-Sah@users.noreply.github.com>
2023-12-21 23:50:29 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+let inp = document.querySelector("input");
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+
+
+btn.addEventListener("click", function(){
+    let item = document.createElement("li");
+    item.innerText = inp.value;
+
+    let delBtn = document.createElement("button");
+    delBtn.innerText = "delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value = " ";
+});
+
+let delBtns = document.querySelectorAll(".delete");
+for(delBtn of delBtns){
+    delBtn.addEventListener("click", function(){
+        let par = this.parentElement;
+        par.remove();
+    });
+}
